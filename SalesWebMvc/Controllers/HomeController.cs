634078ec23f9f7eb6972c623f46dde39900de408
Dennis Martins <dennis.furtado@vibetecnologia.com>
2023-08-21 12:57:37 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace SalesWebMvc.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
-
+            ViewData["Programer"] = "Dennon";
             return View();
         }
 
