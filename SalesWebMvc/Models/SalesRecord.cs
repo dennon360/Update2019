06c783c6eef6a9fc7757697ad7e6cbafb4250dbb
Dennis Martins <dennis.furtado@vibetecnologia.com>
2023-08-21 15:57:59 -0300

    First Model-Controller-View - Department

---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; private set; }
+        public DateTime Date { get; private set; }
+        public double Amount { get; private set; }
+    }
+}
---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public double BaseSalary { get; private set; }
+
+    }
+}
